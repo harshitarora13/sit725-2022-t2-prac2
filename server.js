11aa05d9559dde22daf53d509ed1dc06db8557a9
harshitarora13 <its.harshit.arora@gmail.com>
2022-08-07 02:22:59 +0530

    added themultiplication

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ const addNumbers = (number1, number2) => {
     return result;
 }
 
+const multiplyNumbers = (number1, number2) => {
+    var num1 = parseInt(number1)
+    var num2 = parseInt(number2)
+    var result = num1 * num2;
+    return result;
+}
+
 app.get("/addTwoNumbers",(req,res) => {
     var number1 = req.query.number1;
     var number2 = req.query.number2;
@@ -23,6 +30,13 @@ app.post("/addTwoNumbers",(req,res) => {
     var number1 = req.body.number1;
     var number2 = req.body.number2;
     var result = addNumbers(number1,number2)
+    res.json({statusCode: 200, data: result, message:"Success"})
+})
+
+app.post("/multiplyTwoNumbers",(req,res) => {
+    var number1 = req.query.number1;
+    var number2 = req.query.number2;
+    var result = multiplyNumbers(number1,number2)
     res.json({statusCode: 200, data: result, message:"Success"})
 })
 
